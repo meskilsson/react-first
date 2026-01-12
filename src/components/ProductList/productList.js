058815c53bfcba04a32c5d04fbafe41b2e100817
Mattias Eskilsson <mattiaseskilsson@hotmail.se>
2026-01-12 14:37:23 +0100

    alot of moose pictures

--- src/components/ProductList/productList.js
+++ src/components/ProductList/productList.js
@@ -1,12 +1,12 @@
 
 
 const products = [
-    { id: 'nike-id', name: 'nike shoe', price: 49.99, category: 'shoes', img: null, inStock: false },
-    { id: 'adidas-id', name: 'adidas shirt', price: 4.99, category: 'shirt', img: null, inStock: false },
-    { id: 'puma-id', name: 'puma shoe', price: 29.99, category: 'shoes', img: null, inStock: true },
-    { id: 'underarmour-id', name: 'under armour pants', price: 7.99, category: 'pants', img: null, inStock: true },
-    { id: 'peak-id', name: 'peak jacket', price: 99.99, category: 'jacket', img: null, inStock: true },
-    { id: 'haglof-id', name: 'haglof fleece', price: 8.99, category: 'fleece', img: null, inStock: true },
+    { id: 'nike-id', name: 'nike shoe', price: 49.99, category: 'shoes', img: '/imgs/alg.jpg', inStock: false },
+    { id: 'adidas-id', name: 'adidas shirt', price: 4.99, category: 'shirt', img: '/imgs/alg.jpg', inStock: false },
+    { id: 'puma-id', name: 'puma shoe', price: 29.99, category: 'shoes', img: '/imgs/alg.jpg', inStock: true },
+    { id: 'underarmour-id', name: 'under armour pants', price: 7.99, category: 'pants', img: '/imgs/alg.jpg', inStock: true },
+    { id: 'peak-id', name: 'peak jacket', price: 99.99, category: 'jacket', img: '/imgs/alg.jpg', inStock: true },
+    { id: 'haglof-id', name: 'haglof fleece', price: 8.99, category: 'fleece', img: '/imgs/alg.jpg', inStock: true },
 ];
 
 
--- src/components/ProfileCard/index.js
+++ src/components/ProfileCard/index.js
@@ -1,9 +1,9 @@
 
 
 const profiles = [
-    { id: 'mattias-id', name: 'Mattias', title: 'Developer', description: 'Aspiring junior developer', img: 'img', isSelectable: true },
-    { id: 'susanne-id', name: 'Susanne', title: 'Nurse', description: 'Mother', img: 'img', isSelectable: false },
-    { id: 'jakob-id', name: 'Jakob', title: 'Manager', description: 'Bestfriend', img: 'img', isSelectable: true }
+    { id: 'mattias-id', name: 'Mattias', title: 'Developer', description: 'Aspiring junior developer', img: '/imgs/alg.jpg', isSelectable: true },
+    { id: 'susanne-id', name: 'Susanne', title: 'Nurse', description: 'Mother', img: '/imgs/alg.jpg', isSelectable: false },
+    { id: 'jakob-id', name: 'Jakob', title: 'Manager', description: 'Bestfriend', img: '/imgs/alg.jpg', isSelectable: true }
 ];
 
 
